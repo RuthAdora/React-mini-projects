006b1fa25205708669a3cf8da3305d62622e2173
RuthAdora <adoraruth01@hmail.com>
2024-09-17 12:06:11 +0300

    create shop cart functionality frontend

--- food-del/frontend/src/context/StoreContext.jsx
+++ food-del/frontend/src/context/StoreContext.jsx
@@ -10,16 +10,30 @@ const StoreContextProvider = (props) => {
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
-  const addToCart = (itemId) => {
+  const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+    if (token) {
+      await axios.post(
+        url + "/api/cart/add",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
-  const removeFromCart = (itemId) => {
+  const removeFromCart = async (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      await axios.post(
+        url + "/api/cart/remove",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
   const getTotalCartAmount = () => {
     let totalAmount = 0;
@@ -33,28 +47,25 @@ const StoreContextProvider = (props) => {
   };
 
   const fetchFoodList = async () => {
-    //console.log(res);
-
     const res = await axios.get(url + "/api/food/list");
 
     setFoodList(res.data.data);
   };
-
-  /*useEffect(() => {
-    async function loadData() {
-      await fetchFoodList();
-      if (localStorage.getItem("token")) {
-        setToken(localStorage.getItem("token"));
-      }
-    }
-    loadData();
-  }, []);*/
+  const loadCartData = async (token) => {
+    const res = await axios.post(
+      url + "/api/cart/get",
+      {},
+      { headers: { token } }
+    );
+    setCartItems(res.data.cartData);
+  };
 
   useEffect(() => {
     async function loadData() {
       await fetchFoodList();
       if (localStorage.getItem("token")) {
         setToken(localStorage.getItem("token"));
+        await loadCartData(localStorage.getItem("token"));
       }
     }
     loadData();
